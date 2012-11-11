77773ff27adc424d0327231a655f4afcf410b1cd
Thomas Schreiber <tom@insatsu.us>
2012-11-11 02:09:02 +0100

    as of 11.11.12 this repo can be found at https://github.com/nillab/dotfiles/

--- bash/main.sh
+++
@@ -1,9 +0,0 @@
-export DOTFILES=$HOME/dotfiles
-
-. "$DOTFILES/bash/env.sh"
-. "$DOTFILES/bash/config.sh"
-. "$DOTFILES/bash/aliases.sh"
-
-if [ -f "$HOME/.private" ]; then
-    . "$HOME/.private"
-fi
